f2d2bc5294064cc7265e40b03e7574b00dcbb40b
Alana2005 <70655752+Alana2005@users.noreply.github.com>
2020-09-03 12:49:58 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# IsTouching-Algorithim-Design
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,31 @@
+var fixedRect,movingRect; 
+
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect=createSprite(600,400,100,140);
+  movingRect=createSprite(300,200,100,140);
+
+  fixedRect.shapeColor='green';
+  movingRect.shapeColor='green';
+}
+
+function draw() {
+  background(0);  
+
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+  if (movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2 
+    && fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2
+    && movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2
+    && fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2){
+    fixedRect.shapeColor='red';
+    movingRect.shapeColor='red';
+  }
+  else{
+    fixedRect.shapeColor='green';
+    movingRect.shapeColor='green';
+  }
+  drawSprites();
+}
